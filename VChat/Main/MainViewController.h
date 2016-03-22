8c6a2eca542d6a254e69a77be9aa28166e0653ff
Recover <787321523@qq.com>
2016-03-23 02:01:42 +0800

    搭建主界面

---
+++ VChat/Main/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  VChat
+//
+//  Created by Recover on 16/3/23.
+//  Copyright © 2016年 Recover. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UITabBarController
+
+@end
